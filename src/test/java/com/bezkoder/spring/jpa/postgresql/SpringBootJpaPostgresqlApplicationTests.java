e690c218bf9be7c702f75e005d9c49362c90fbeb
Nitin Rawat <37417164+NitinRawat99@users.noreply.github.com>
2025-08-10 19:49:49 +0530

    Update SpringBootJpaPostgresqlApplicationTests.java

--- src/test/java/com/bezkoder/spring/jpa/postgresql/SpringBootJpaPostgresqlApplicationTests.java
+++ src/test/java/com/bezkoder/spring/jpa/postgresql/SpringBootJpaPostgresqlApplicationTests.java
@@ -3,11 +3,11 @@ package com.bezkoder.spring.jpa.postgresql;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringBootJpaPostgresqlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
